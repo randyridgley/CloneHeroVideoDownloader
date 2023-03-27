5dd6408d763b79920353360d6eade564640089ad
Randy Ridgley <rridgley@amazon.com>
2023-03-27 09:41:52 -0400

    change options

--- VideoDownload.py
+++ VideoDownload.py
@@ -6,13 +6,13 @@ import yt_dlp
 from googlesearch import search 
 from tqdm import tqdm
 
-def clean_cookie():
-	if os.path.exists(".google-cookie"):
-		os.remove(".google-cookie")
+# def clean_cookie():
+# 	if os.path.exists(".google-cookie"):
+# 		os.remove(".google-cookie")
 
 homeFolder = os.path.abspath(os.getcwd() + "\\songs")
 os.chdir(homeFolder)
-clean_cookie()
+# clean_cookie()
 
 i=0
 for filename in glob.iglob(homeFolder + "/**/song.ini", recursive=True):
@@ -27,29 +27,39 @@ with tqdm(total=i,unit="videos") as pbar:
 		pbar.update(1)
 
 		if not os.path.exists("video.mp4"):
-			query = 'Youtube {} (Official Music Video)'.format(currentSongName)
+			try:
+				query = 'Youtube {} (Official Music Video)'.format(currentSongName)
 
-			# scrapes the URL from google
-			for j in search(query, tld="com", lang='en', num=1, start=0, stop=1):
-				url = j;
+				# scrapes the URL from google
+				for j in search(query, tld="com", lang='en', num=1, start=0, stop=1):
+					url = j;
 
-			# downloads the song
-			ydl_opts = {'outtmpl': 'video.mp4',
-					'format': 'mp4',
-					'nooverwrites': 0,
-					'noplaylist': 1,
-					'quiet': True}
-			with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-				ydl.download([url])
+				# downloads the song
+				ydl_opts = {
+				'outtmpl': 'video.mp4',
+				'format': 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best',
+				'continue' : True,
+				'verbose' : True,
+				'no-overwrites' : True,
+				'noplaylist' : True,
+				'geo-bypass' : True,
+				'nocheckcertificate': True,
+				'ignoreerrors': True,
+				'quiet': True,
+				}			
+				with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+					ydl.download([url])
+			except Exception:
+				continue
 
-			# change the song.ini file to attempt to sync the video
-			config = configparser.ConfigParser()
-			config.read('song.ini')
-			config.set('song', 'video_start_time', '-3000')
+			# # change the song.ini file to attempt to sync the video
+			# config = configparser.ConfigParser()
+			# config.read('song.ini')
+			# config.set('song', 'video_start_time', '-3000')
 
-			with open('song.ini', 'w') as configfile:
-				config.write(configfile)
+			# with open('song.ini', 'w') as configfile:
+			# 	config.write(configfile)
 
-			clean_cookie()
+			# clean_cookie()
 
 input("Video download complete.  Press Enter to exit.")
